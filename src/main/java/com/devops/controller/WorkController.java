870062fcd01589dff4398437007746c3d0956964
lamiskid <aboladelawal2019@gmail.com.com>
2024-07-14 18:20:32 +0100

    to docker

--- src/main/java/com/devops/controller/WorkController.java
+++ src/main/java/com/devops/controller/WorkController.java
@@ -1,9 +1,11 @@
 package com.devops.controller;
 
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Data
 public class WorkController {
 
 }

ba97a5cc0ad75349a34bfae8c2213ddeeee182b1
lamiskid <aboladelawal2019@gmail.com.com>
2024-07-14 04:26:30 +0100

    fixes

--- src/main/java/com/devops/controller/WorkController.java
+++ src/main/java/com/devops/controller/WorkController.java
@@ -1,2 +1,9 @@
-package com.devops.controller;public class WorkContoller {
+package com.devops.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WorkController {
+
 }
